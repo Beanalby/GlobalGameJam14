175658be11735ccc6631c53dc60cf19aad2a2d62
Jason Viers <bean@beanalby.net>
2014-01-24 23:37:41 -0500

    Added world switching

---
+++ Assets/Scripts/WorldState.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum GameWorld { human, race, dino, space };
+
+public class WorldState : MonoBehaviour {
+
+    private GameWorld world;
+    public GameWorld World {
+        get { return world; }
+    }
+
+	void Start () {
+        world = GameWorld.human;
+	}
+	
+    public void Update() {
+        if(Input.GetKeyDown(KeyCode.A)) {
+            UpdateWorld(GameWorld.human);
+        } else if(Input.GetKeyDown(KeyCode.S)) {
+            UpdateWorld(GameWorld.race);
+        } else if(Input.GetKeyDown(KeyCode.D)) {
+            UpdateWorld(GameWorld.dino);
+        } else if(Input.GetKeyDown(KeyCode.F)) {
+            UpdateWorld(GameWorld.space);
+        }
+    }
+
+    public void UpdateWorld(GameWorld newWorld) {
+        this.world = newWorld;
+        foreach (WorldSwitcher obj in GameObject.FindObjectsOfType<WorldSwitcher>()) {
+            obj.WorldSwitched(newWorld);
+        }
+    }
+}
---
+++ Assets/Scripts/WorldSwitcher.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldSwitcher : MonoBehaviour {
+
+    public GameObject humanVersion, raceVersion, dinoVersion, spaceVersion;
+
+    public void WorldSwitched(GameWorld world) {
+        Debug.Log("Switching to " + world);
+        GameObject current = transform.parent.gameObject;
+        transform.parent = null;
+        transform.position = current.transform.position;
+        transform.rotation = current.transform.rotation;
+        Debug.Log("Destroying " + current.name);
+        Destroy(current);
+        GameObject prefab = null;
+        switch (world) {
+            case GameWorld.human:
+                prefab = humanVersion; break;
+            case GameWorld.race:
+                prefab = raceVersion; break;
+            case GameWorld.dino:
+                prefab = dinoVersion; break;
+            case GameWorld.space:
+                prefab = spaceVersion; break;
+        }
+        current = Instantiate(prefab) as GameObject;
+        current.transform.position = transform.position;
+        current.transform.rotation = transform.rotation;
+        // if it has a switcher, delete ourselves
+        Transform otherSwitcher = transform.Find("switcher");
+        if (otherSwitcher != null) {
+            //Destroy(gameObject);
+        } else {
+            transform.parent = current.transform;
+        }
+    }
+}
