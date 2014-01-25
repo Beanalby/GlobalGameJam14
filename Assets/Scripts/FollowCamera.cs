2884ebe18bc59fd9d5fec2f48c1f37a1b8401b3b
Jason Viers <bean@beanalby.net>
2014-01-25 13:34:01 -0500

    Added terrain & basic 3rd person controller

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCamera : MonoBehaviour {
+    private GameObject target = null;
+    private float distance = 3f;
+    private float height = 2f;
+
+    private float angularSmoothLag = 0.1f;
+    private float angularMaxSpeed = 255.0f;
+    private float angleVelocity = 0;
+    private float heightVelocity = 0;
+    private float heightSmoothLag = 0.3f;
+
+    public void Update() {
+        if (target == null) {
+            target = GameObject.FindGameObjectWithTag("Player");
+            if (target == null) {
+                Debug.LogError("No player to follow");
+                return;
+            }
+        }
+
+        // adjust our rotation
+        float targetAngle = target.transform.eulerAngles.y;
+        float currentAngle = transform.eulerAngles.y;
+        currentAngle = Mathf.SmoothDampAngle(currentAngle, targetAngle,
+            ref angleVelocity, angularSmoothLag);
+        Quaternion currentRotation = Quaternion.Euler(0, currentAngle, 0);
+
+        float targetHeight = target.transform.position.y + height;
+        float currentHeight = transform.position.y;
+        currentHeight = Mathf.SmoothDamp(currentHeight, targetHeight,
+            ref heightVelocity, heightSmoothLag);
+
+        Vector3 pos = target.transform.position;
+        pos += currentRotation * Vector3.back * distance;
+        pos.y = currentHeight;
+        transform.position = pos;
+        transform.rotation = Quaternion.LookRotation(target.transform.position - transform.position);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,4 +2,21 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
+
+    private float speed = 5;
+    private float turnSpeed = 75;
+
+    public void Update() {
+        Vector3 rot = rigidbody.rotation.eulerAngles;
+        rot.y += Input.GetAxis("Horizontal") * Time.deltaTime * turnSpeed;
+        rigidbody.MoveRotation(Quaternion.Euler(rot));
+
+        if(Input.GetAxis("Vertical") != 0) {
+            Vector3 pos = rigidbody.position;
+            //Debug.Log("Transformed is " + transform.TransformDirection(Vector3.forward)
+            //    + ", doing " + (transform.TransformDirection(Vector3.forward) * Input.GetAxis("Vertical") * speed));
+            pos += transform.TransformDirection(Vector3.forward) * Input.GetAxis("Vertical") * Time.deltaTime * speed;
+            rigidbody.MovePosition(pos);
+        }
+    }
 }
