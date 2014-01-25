609372c52eebd774243187830dbe2bb620579445
Jason Viers <bean@beanalby.net>
2014-01-25 17:42:38 -0500

    Added space terrain, all switching

---
+++ Assets/Scripts/ExtraSwitcher.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExtraSwitcher : MonoBehaviour {
+    public GameObject[] human, race, dino, space;
+
+    public GameObject[] current;
+
+    public void Start() {
+        current = human;
+        foreach (GameObject obj in current) {
+            obj.SetActive(true);
+        }
+    }
+
+    public void UpdateWorld(GameWorld world) {
+        foreach (GameObject obj in current) {
+            obj.SetActive(false);
+        }
+        switch (world) {
+            case GameWorld.human:
+                current = human; break;
+            case GameWorld.race:
+                current = race; break;
+            case GameWorld.dino:
+                current = dino; break;
+            case GameWorld.space:
+                current = space; break;
+        }
+        foreach (GameObject obj in current) {
+            obj.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/TerrainSwitcher.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TerrainSwitcher : MonoBehaviour {
+    public GameObject human, race, dino, space;
+
+    private GameObject current;
+
+    public void Start() {
+        current = human;
+        human.SetActive(true);
+    }
+
+    public void UpdateWorld(GameWorld world) {
+        if (current) {
+            current.SetActive(false);
+        }
+        switch (world) {
+            case GameWorld.human:
+                current = human; break;
+            case GameWorld.race:
+                current = race; break;
+            case GameWorld.dino:
+                current = dino; break;
+            case GameWorld.space:
+                current = space; break;
+        }
+        if (current) {
+            current.SetActive(true);
+        }
+    }
+}
