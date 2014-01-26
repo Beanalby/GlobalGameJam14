28e063581f82e3ea980295a0277a55911e309ee1
Jason Viers <bean@beanalby.net>
2014-01-26 13:59:37 -0500

    Added title screen

---
+++ Assets/Scripts/TitleDriver.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleDriver : MonoBehaviour {
+    public Texture2D titleImage;
+
+    public void Update() {
+        if (Input.GetButtonDown("Jump")) {
+            Application.LoadLevel("Stage1");
+        }
+    }
+    public void OnGUI() {
+        GUI.DrawTexture(
+            new Rect( Screen.width / 2 - titleImage.width / 2,
+                Screen.height / 2 - titleImage.height / 2,
+                titleImage.width, titleImage.height),
+            titleImage);
+    }
+}
