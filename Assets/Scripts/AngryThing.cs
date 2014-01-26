c6fcf1205bf8c1cabc5444fd2dec7ca6b8b113b5
Jason Viers <bean@beanalby.net>
2014-01-25 21:23:03 -0500

    Added AngryThing

---
+++ Assets/Scripts/AngryThing.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AngryThing : MonoBehaviour {
+    public float angryRange = 8f;
+    public bool isX = false;
+
+    float lastCheck = -1, checkCooldown = .25f;
+    int playerMask;
+
+    private GameObject target = null;
+
+    public void Start() {
+        playerMask = 1 << LayerMask.NameToLayer("Player");
+    }
+    public void Update() {
+        UpdateTarget();
+        HandleMovement();
+    }
+
+    private void UpdateTarget() {
+        if(Time.time - lastCheck > checkCooldown) {
+            if (target == null) {
+                // see if they're in range
+                Collider[] hits;
+                hits = Physics.OverlapSphere(transform.position, angryRange,
+                    playerMask);
+                if (hits.Length != 0) {
+                    target = hits[0].gameObject;
+                }
+            } else {
+                // see if they've left range
+                float distance = (target.transform.position - transform.position).magnitude;
+                if (distance > angryRange) {
+                    target = null;
+                }
+            }
+        }
+    }
+
+    private void HandleMovement() {
+        if (target == null) {
+            return;
+        }
+
+        // move towards the player
+        Vector3 dest = transform.position;
+        if (isX) {
+            dest.x = target.transform.position.x;
+        } else {
+            dest.z = target.transform.position.z;
+        }
+        transform.position = Vector3.Lerp(transform.position, dest, .1f);
+    }
+
+    public void OnCollisionEnter(Collision collision) {
+        Debug.Log(name + " collided with " + collision.gameObject.name);
+        if (collision.gameObject.tag == "Player") {
+            collision.gameObject.SendMessage("GotHit", gameObject);
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,14 +5,20 @@ public class PlayerController : MonoBehaviour {
 
     private float speed = 5;
     private float turnSpeed = 75;
+    private float throwStrength = 5f;
 
     private WorldControl wc;
     private bool needsCentered = true;
+
+    private bool canControl = true;
 
     public void Start() {
         wc = GameObject.Find("WorldState").GetComponent<WorldControl>();
     }
     public void Update() {
+        if (!canControl) {
+            return;
+        }
         if (wc.showingMenu) {
             needsCentered = true;
             return;
@@ -32,6 +38,24 @@ public class PlayerController : MonoBehaviour {
             Vector3 pos = rigidbody.position;
             pos += transform.TransformDirection(Vector3.forward) * Input.GetAxis("Vertical") * Time.deltaTime * speed;
             rigidbody.MovePosition(pos);
+        }
+    }
+
+    public void GotHit(GameObject attacker) {
+        // dino doesn't get thrown
+        if (GameObject.Find("WorldState").GetComponent<WorldState>().world == GameWorld.dino) {
+            return;
+        }
+        Vector3 throwDir = attacker.transform.TransformDirection(Vector3.forward);
+        //Vector3 throwDir = attacker.transform.rotation.eulerAngles.normalized;
+        throwDir.y = 1;
+        rigidbody.velocity = throwDir * throwStrength;
+        canControl = false;
+    }
+
+    public void OnCollisionEnter(Collision collision) {
+        if (!canControl) {
+            canControl = true;
         }
     }
 }
