1c49dd4ab982551ebe732a1d3f27a28cee00b8ab
Jason Viers <bean@beanalby.net>
2014-01-26 14:16:07 -0500

    Now detects when all boxes are found

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -3,16 +3,28 @@ using System.Collections;
 
 public class GameState : MonoBehaviour {
     private int pickups = 0;
+    bool finished = false;
     public int Pickups {
         get { return pickups; }
     }
 
     public void AddPickup() {
         pickups++;
+        // check if there are any pickups left
+        foreach(Pickup p in GameObject.FindObjectsOfType<Pickup>()) {
+            if (!p.pickedUp) {
+                return;
+            }
+        }
+        // nothing left to be picked up
+        finished = true;
     }
 
     public void OnGUI() {
-        GUI.Label(new Rect(10, 10, 200, 100), "Juice boxes: " + pickups);
+        if (!finished) {
+            GUI.Label(new Rect(10, 10, 200, 100), "Juice boxes: " + pickups);
+        } else {
+            GUI.Label(new Rect(10, 10, 200, 100), "All Juice boxes found!");
+        }
     }
 }
-
--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -3,10 +3,13 @@ using System.Collections;
 
 public class Pickup : MonoBehaviour {
 
+    public bool pickedUp = false;
+
     public void OnTriggerEnter(Collider other) {
         if(other.tag != "Player") {
             return;
         }
+        pickedUp = true;
         GameObject.Find("WorldState").GetComponent<GameState>().AddPickup();
         Destroy(gameObject);
     }
